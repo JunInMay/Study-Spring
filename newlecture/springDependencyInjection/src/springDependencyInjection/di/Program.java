be63e42a1edd8fa8cb9574ea5f7ec182b52401b0
Chana <twszak01@gmail.com>
2024-10-21 01:55:50 +0900

    [2024-10-20] 8강 중

--- newlecture/springDependencyInjection/src/springDependencyInjection/di/Program.java
+++ newlecture/springDependencyInjection/src/springDependencyInjection/di/Program.java
@@ -1,22 +1,18 @@
 package springDependencyInjection.di;
 
-import springDependencyInjection.di.entity.Exam;
-import springDependencyInjection.di.entity.NewlecExam;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
 import springDependencyInjection.di.ui.ExamConsole;
 import springDependencyInjection.di.ui.GridExamConsole;
-import springDependencyInjection.di.ui.InlineExamConsole;
 
 public class Program {
 
 	public static void main(String[] args) {
-		Exam exam = new NewlecExam();
-		
-		// inline : 한 줄로 출력하는 예제
-//		ExamConsole console = new InlineExamConsole(exam);
-		// grid : 그리드로 출력하는 예제
+		ApplicationContext context = new ClassPathXmlApplicationContext("springDependencyInjection/di/setting.xml");
 
-		ExamConsole console = new GridExamConsole(exam);
-		console.setExam(exam);
+		//ExamConsole console = (GridExamConsole) context.getBean("console");
+		ExamConsole console = context.getBean(ExamConsole.class);
 		
 		console.print();
 		
